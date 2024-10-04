d3e1d16c4e114e2c0b18bcf7e9cea46bd61e0592
techshalinii <shalinikumari9142@gmail.com>
2024-10-04 21:52:22 +0530

    animated-tool-tip for instructors section

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -21,8 +21,7 @@ export const AnimatedTooltip = ({
 }) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const springConfig = { stiffness: 100, damping: 5 };
-  const x = useMotionValue(0); // going to set this value on mouse move
-  // rotate the tooltip
+  const x = useMotionValue(0); 
   const rotate = useSpring(
     useTransform(x, [-100, 100], [-45, 45]),
     springConfig
@@ -34,7 +33,7 @@ export const AnimatedTooltip = ({
   );
   const handleMouseMove = (event: any) => {
     const halfWidth = event.target.offsetWidth / 2;
-    x.set(event.nativeEvent.offsetX - halfWidth); // set the x value, which is then used in transform and rotate
+    x.set(event.nativeEvent.offsetX - halfWidth); 
   };
 
   return (
