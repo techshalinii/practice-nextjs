9b4961a7b2a1a94f710adbfb25622431f9a3ee4f
techshalinii <shalinikumari9142@gmail.com>
2024-10-04 21:46:38 +0530

    added all-courses in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,11 @@ function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
   return (
     <div
-    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
       <Menu setActive={setActive}>
         <Link href={"/"}>
             <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
         </Link>
-
         <MenuItem setActive={setActive} active={active} item="Our Courses">
            <div className="flex flex-col space-y-4 text-sm">
                 <HoveredLink href="/courses">All courses</HoveredLink>
