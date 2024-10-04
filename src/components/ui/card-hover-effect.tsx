3a36db4201c54b43c85b265e910da06004cdc2a0
techshalinii <shalinikumari9142@gmail.com>
2024-10-04 21:56:23 +0530

    card-hover effect used in webinar section

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -103,8 +103,7 @@ export const CardDescription = ({
       className={cn(
         "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
         className
-      )}
-    >
+      )}>
       {children}
     </p>
   );
