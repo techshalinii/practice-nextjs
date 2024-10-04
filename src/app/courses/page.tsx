ec2a8e670ba537509031bdbe706a2b98ed0bd0e8
techshalinii <shalinikumari9142@gmail.com>
2024-10-04 21:13:50 +0530

    added courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -12,17 +12,10 @@ function page() {
             {courseData.courses.map((course) => (
                 <CardContainer className="inter-var m-4">
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                  <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                  >
+                  <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white" >
                     {course.title}
                   </CardItem>
-                  <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                  >
+                  <CardItem as="p" translateZ="60" className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300" >
                     {course.description}
                   </CardItem>
                   <CardItem translateZ="100" className="w-full mt-4">
@@ -57,5 +50,4 @@ function page() {
     </div>
   )
 }
-
-export default page
+export default page;
