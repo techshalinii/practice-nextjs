8316a60be652dc703abbe5b490e0e9f6582d4a53
techshalinii <shalinikumari9142@gmail.com>
2024-10-05 19:06:38 +0530

    removed unnecessary import

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -2,7 +2,6 @@
 import courseData from "../data/music_courses.json"
 import Link from "next/link"
 import { BackgroundGradient } from "./ui/background-gradient"
-import { div, p } from "framer-motion/client"
 
 interface Course{
     id: number,
@@ -42,7 +41,6 @@ export const FeaturedCourses = () => {
                 ))}</div>
         </div>
         <div className="mt-20 text-center">
-
         <Link href={"/courses"} className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"> View All courses</Link>
         </div>
     </div>
